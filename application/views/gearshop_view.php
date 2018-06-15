3292d097bc5c35bfaae262c9a93b2a47eafb2edd
ostar0816 <ostar7551@gmail.com>
2018-06-15 02:22:23 -0400

    remove base_url() functions

--- application/views/gearshop_view.php
+++ application/views/gearshop_view.php
@@ -6,16 +6,16 @@
   <title>gearShop</title>
   <meta content="gearShop" property="og:title">
   <meta content="width=device-width, initial-scale=1" name="viewport">
-  <link href="<?php echo base_url(); ?>css/normalize.css" rel="stylesheet" type="text/css">
-  <link href="<?php echo base_url(); ?>css/components.css" rel="stylesheet" type="text/css">
-  <link href="<?php echo base_url(); ?>css/crooklynmain.css" rel="stylesheet" type="text/css">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/components.css" rel="stylesheet" type="text/css">
+  <link href="css/crooklynmain.css" rel="stylesheet" type="text/css">
   <link href="css/eqproj.webflow.css" rel="stylesheet" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js" type="text/javascript"></script>
   <script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Great Vibes:400","Merriweather:300,300italic,400,400italic,700,700italic,900,900italic","Lato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic","Permanent Marker:regular"]  }});</script>
   <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
   <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
-  <link href="<?php echo base_url(); ?>images/logo-cc32.png" rel="shortcut icon" type="image/x-icon">
-  <link href="<?php echo base_url(); ?>images/logo-cc256.png" rel="apple-touch-icon">
+  <link href="images/logo-cc32.png" rel="shortcut icon" type="image/x-icon">
+  <link href="images/logo-cc256.png" rel="apple-touch-icon">
   <script src="https://code.jquery.com/jquery-3.2.0.js" integrity="sha256-wPFJNIFlVY49B+CuAIrDr932XSb6Jk3J1M22M3E2ylQ=" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
   <style>
@@ -93,7 +93,7 @@
     </div>
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" type="text/javascript"></script>
-  <script src="<?php echo base_url(); ?>js/crooklynmain.js" type="text/javascript"></script>
+  <script src="js/crooklynmain.js" type="text/javascript"></script>
   <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
 </body>
 </html>
