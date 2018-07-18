96effd9f18a2548007f001178a889e885e5efc8e
ostar0816 <ostar7551@gmail.com>
2018-07-18 16:00:53 -0400

    show gearshop on the iframe

--- application/views/gearshop_view.php
+++ application/views/gearshop_view.php
@@ -6,94 +6,13 @@
   <title>gearShop</title>
   <meta content="gearShop" property="og:title">
   <meta content="width=device-width, initial-scale=1" name="viewport">
-  <link href="<?php echo base_url(); ?>css/normalize.css" rel="stylesheet" type="text/css">
-  <link href="<?php echo base_url(); ?>css/components.css" rel="stylesheet" type="text/css">
-  <link href="<?php echo base_url(); ?>css/crooklynmain.css" rel="stylesheet" type="text/css">
   <link href="<?php echo base_url(); ?>css/eqproj.webflow.css" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js" type="text/javascript"></script>
-  <script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Great Vibes:400","Merriweather:300,300italic,400,400italic,700,700italic,900,900italic","Lato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic","Permanent Marker:regular"]  }});</script>
-  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
-  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
   <link href="<?php echo base_url(); ?>images/logo-cc32.png" rel="shortcut icon" type="image/x-icon">
   <link href="<?php echo base_url(); ?>images/logo-cc256.png" rel="apple-touch-icon">
-  <script src="https://code.jquery.com/jquery-3.2.0.js" integrity="sha256-wPFJNIFlVY49B+CuAIrDr932XSb6Jk3J1M22M3E2ylQ=" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
-  <style>
-  ::-webkit-scrollbar {
-    width: 10px;
-}
-::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
-    border-radius: 10px;
-}
-::-webkit-scrollbar-thumb {
-    border-radius: 10px;
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
-}
-  ::-webkit-scrollbar-corner       { display: none; height: 0px; width: 0px; }
-  .SCROLLABLE-Div-Class-Name {
-    overflow-x: hidden;}
-</style>
-  <!--  Facebook Pixel Code  -->
-  <script>
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1220794488024725');
-  fbq('track', 'PageView');
-</script>
-  <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1220794488024725&ev=PageView&noscript=1"></noscript>
-  <!--  End Facebook Pixel Code  -->
-  <style>
-  ::-webkit-scrollbar {
-    width: 10px;
-}
-::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
-    border-radius: 10px;
-}
-::-webkit-scrollbar-thumb {
-    border-radius: 10px;
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
-}
-  ::-webkit-scrollbar-corner       { display: none; height: 0px; width: 0px; }
-  .SCROLLABLE-Div-Class-Name {
-    overflow-x: hidden;}
-</style>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.cycle2/2.1.6/jquery.cycle2.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/1.3.7/wavesurfer.min.js"></script>
-  <script src="https://npmcdn.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
 </head>
-<body>
-  <div data-collapse="medium" data-animation="default" data-duration="400" class="navbarmain w-nav">
-    <a href="home" class="brand-link w-nav-brand">
-      <h1 class="logo"></h1>
-    </a>
-    <div class="fb w-widget w-widget-facebook"><iframe src="https://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fcrooklynclan&amp;layout=button_count&amp;locale=en_US&amp;action=like&amp;show_faces=false&amp;share=false" scrolling="no" frameborder="0" allowtransparency="true" style="border: none; overflow: hidden; width: 225px; height: 35px;"></iframe></div>
-    <nav role="navigation" class="w-nav-menu"><a href="aboutus" class="nav-link-2 w-nav-link">Home</a><a href="javascript:history.back();" class="nav-link-2 w-nav-link">Back</a></nav>
-    <div class="menu-button-3 w-nav-button">
-      <div class="w-icon-nav-menu"></div>
-    </div>
+<body style="margin: 0; overflow: hidden;">
+  <div class="iframe-container">
+    <iframe src="https://v2-docs.crooklynclan.net/newsetup/gearshop" scrolling="no" frameborder="0" style="width: 100%; height: 100%;"></iframe>
   </div>
-  <div class="flexwrapper">
-    <div class="titlertopper">o<span class="text-span-35"><strong xmlns="http://www.w3.org/1999/xhtml">o<span class="text-span-36">o</span></strong>
-      </span>baby.</div>
-    <div class="mystore w-embed w-script">
-      <div id="my-store-11611379"></div>
-      <div>
-        <script type="text/javascript" src="https://app.ecwid.com/script.js?11611379&data_platform=code&data_date=2017-05-31" charset="utf-8"></script>
-        <script type="text/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-11611379");</script>
-      </div>
-    </div>
-  </div>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" type="text/javascript"></script>
-  <script src="<?php echo base_url(); ?>js/crooklynmain.js" type="text/javascript"></script>
-  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
 </body>
 </html>
