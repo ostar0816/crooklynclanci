0ced1f9aeda2e72d92ecd4c1706fa57cf6a8b233
ostar0816 <ostar7551@gmail.com>
2018-07-24 00:28:28 -0400

    set redirection of editor ads

--- js/slider_show.js
+++ js/slider_show.js
@@ -28,8 +28,10 @@ function showDivs(n) {
     if (n < 1) {slideIndex = x.length}
     for (i = 0; i < x.length; i++) {
         x[i].style.opacity = "0";
+        x[i].style['z-index'] = "1";
     }
     x[slideIndex-1].style.opacity = "1";
+    x[slideIndex-1].style['z-index'] = "2";
 
     if (!pauseSlide) {
         if (timerId) {
